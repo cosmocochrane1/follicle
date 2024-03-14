45faa23b2d24c35467dd25c5d7e973a011beff06
angryayam <komangwluce@gmail.com>
2024-03-14 12:55:11 +0800

    more work

--- src/components/MarketingPage/Footer.js
+++ src/components/MarketingPage/Footer.js
@@ -6,23 +6,30 @@ import styled from "styled-components";
 
 const Footer = () => {
   return (
-
     <footer className="relative bg-white">
       {/* <WhiteOval /> */}
       <div className="relative w-full py-6 border-t rounded-tl-[50px] pb-[140px] pt-[90px] rounded-tr-[50px] bg-tertiary">
-        <FooterBlobContainer className={'absolute w-full overflow-hidden bottom-0 left-0'}>
+        <div className={"absolute w-full overflow-hidden bottom-0 left-0"}>
           <div>
-            <img src="/footer-blob.png" alt="blob" />
+            <div className="w-screen">
+              <img
+                src="/footer-blob.png"
+                alt="blob"
+                className="w-full contain"
+              />
+            </div>
             <p className="absolute left-0 right-0 m-auto text-sm font-light text-center text-white bottom-5">
               Copyright 2023 Logo. All rights reserved.
             </p>
           </div>
-        </FooterBlobContainer>
+        </div>
 
         <div className="container flex flex-col items-center justify-between w-full mx-auto">
-          <div className={'flex mb-[100px] w-full flex-col text-center'}>
-            <h1 className="text-[51px] text-white">Get The Latest</h1>
-            <p className="text-sm text-white mt-[35px] mb-[55px]">
+          <div className={"flex mb-[100px] w-full flex-col text-center"}>
+            <h2 className="font-extrabold text-5xl font-serif text-background">
+              Get The Latest
+            </h2>
+            <p className="text-sm text-white mt-4 mb-[55px]">
               Sign up for our newsletter
             </p>
             <SignUpInput />
@@ -57,17 +64,31 @@ const Footer = () => {
 
             <div className="flex flex-col gap-7 justfity-between">
               {/* Opening div */}
-              <p className={'text-lg mb-1 text-tertiary-foreground'}>Services</p>
-              <Link href="/" className="text-sm text-tertiary-foregroundSecondary">
+              <p className={"text-lg mb-1 text-tertiary-foreground font-serif"}>
+                Services
+              </p>
+              <Link
+                href="/"
+                className="text-sm text-tertiary-foregroundSecondary"
+              >
                 Treatments
               </Link>
-              <Link href="/auth" className="text-sm text-tertiary-foregroundSecondary">
+              <Link
+                href="/auth"
+                className="text-sm text-tertiary-foregroundSecondary"
+              >
                 Testimonials
               </Link>
-              <Link href="/auth" className="text-sm text-tertiary-foregroundSecondary">
+              <Link
+                href="/auth"
+                className="text-sm text-tertiary-foregroundSecondary"
+              >
                 FAQ's
               </Link>
-              <Link href="/auth" className="text-sm text-tertiary-foregroundSecondary">
+              <Link
+                href="/auth"
+                className="text-sm text-tertiary-foregroundSecondary"
+              >
                 Contact
               </Link>
               {/* Closing div */}
@@ -75,54 +96,71 @@ const Footer = () => {
 
             <div className="flex flex-col gap-7 justfity-between">
               {/* Opening div */}
-              <p className={'text-lg mb-1 text-tertiary-foreground'}>Company</p>
-              <Link href="/" className="text-sm text-tertiary-foregroundSecondary">
+              <p className={"text-lg mb-1 text-tertiary-foreground font-serif"}>
+                Company
+              </p>
+              <Link
+                href="/"
+                className="text-sm text-tertiary-foregroundSecondary"
+              >
                 Help Center
               </Link>
-              <Link href="/auth" className="text-sm text-tertiary-foregroundSecondary">
+              <Link
+                href="/auth"
+                className="text-sm text-tertiary-foregroundSecondary"
+              >
                 privacy policy
               </Link>
-              <Link href="/auth" className="text-sm text-tertiary-foregroundSecondary">
+              <Link
+                href="/auth"
+                className="text-sm text-tertiary-foregroundSecondary"
+              >
                 {/* Opening div */}
                 Terms & Conditions
                 {/* Closing div */}
               </Link>
-              <Link href="/auth" className="text-sm text-tertiary-foregroundSecondary">
+              <Link
+                href="/auth"
+                className="text-sm text-tertiary-foregroundSecondary"
+              >
                 Members
               </Link>
               {/* Closing div */}
             </div>
             <div className="flex flex-col gap-7 justfity-between">
               {/* Opening div */}
-              <p className={'text-lg mb-1 text-tertiary-foreground'}>Contact Information</p>
+              <p className={"text-lg mb-1 text-tertiary-foreground font-serif"}>
+                Contact Information
+              </p>
               <div className="flex flex-row gap-2">
-                <LucideIcon name="mail" className="w-4 h-4 text-tertiary-foregroundSecondary" />
+                <LucideIcon
+                  name="mail"
+                  className="w-4 h-4 text-tertiary-foregroundSecondary"
+                />
                 <p className="text-sm text-tertiary-foregroundSecondary">
                   logo.12498@gmail.com
                 </p>
               </div>
               <div className="flex flex-row gap-2">
-                <LucideIcon name="phone" className="w-4 h-4 text-tertiary-foregroundSecondary" />
+                <LucideIcon
+                  name="phone"
+                  className="w-4 h-4 text-tertiary-foregroundSecondary"
+                />
                 <p className="text-sm text-tertiary-foregroundSecondary">
                   012 345 6789
                 </p>
               </div>
 
-
               {/* Closing div */}
             </div>
-
-
           </div>
-
         </div>
       </div>
-    </footer >
+    </footer>
   );
 };
 
 export default Footer;
-
 
 const WhiteOval = styled.div`
   background: white;
@@ -130,12 +168,4 @@ const WhiteOval = styled.div`
   width: 100px;
   height: 100px;
   border-radius: 100%;
-`
-
-const FooterBlobContainer = styled.div`
-  img {
-    height: auto;
-    width: auto;
-    max-width: fit-content;
-  }
-`
+`;
--- src/components/MarketingPage/SIgnUpInput.js
+++ src/components/MarketingPage/SIgnUpInput.js
@@ -1,50 +1,54 @@
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import styled from "styled-components";
+import { Button } from "../ui/button";
 
 const EmailSignUp = () => {
-    // State for the email value
-    const [email, setEmail] = useState("");
+  // State for the email value
+  const [email, setEmail] = useState("");
 
-    return (
-        <InputContainer className="relative flex w-full mx-auto overflow-hidden bg-white rounded-full">
-            <StyledInput
-                type="email"
-                placeholder="Email address"
-                className="flex-1 w-full h-full text-[18px] p-8 text-sm text-gray-700 placeholder-gray-500 bg-white border-none"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            <button
-                type="submit"
-                className="px-8 text-[18px] w-[150px] m-[8px] py-3 text-sm font-semibold text-white transition-colors duration-200 transform bg-teal-400 border-transparent rounded-full focus:border-transparent focus:ring-0 hover:bg-teal-500 focus:bg-teal-600 focus:outline-none"
-            >
-                Sign Up
-            </button>
-        </InputContainer>
-    );
+  return (
+    <InputContainer className="relative flex w-full mx-auto overflow-hidden bg-white rounded-full items-center pr-8">
+      <StyledInput
+        type="email"
+        placeholder="Email address"
+        className="flex-1 w-full h-full text-[18px] p-8 text-sm text-gray-700 placeholder-gray-500 bg-white border-none"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Button type="submit">Sign Up</Button>
+      {/* <button
+        type="submit"
+        className="px-8 text-[18px] w-[150px] m-[8px] py-3 text-sm font-semibold text-white transition-colors duration-200 transform bg-teal-400 border-transparent rounded-full focus:border-transparent focus:ring-0 hover:bg-teal-500 focus:bg-teal-600 focus:outline-none"
+      >
+        Sign Up
+      </button> */}
+    </InputContainer>
+  );
 };
 
 export default EmailSignUp;
 
 const InputContainer = styled.div`
-    max-width: 660px;
-    height: 72px;
-    width: 100%;
-    font-size: 18px;
-    
-    input {
-        outline: none !important;
-        &:focus, &:active {
-            outline: none !important;
-        }
-    }
+  max-width: 660px;
+  height: 72px;
+  width: 100%;
+  font-size: 18px;
 
-textarea:focus, input:focus{
+  input {
     outline: none !important;
-}
-`
+    &:focus,
+    &:active {
+      outline: none !important;
+    }
+  }
+
+  textarea:focus,
+  input:focus {
+    outline: none !important;
+  }
+`;
 
 const StyledInput = styled(Input)`
-    font-size: 18px !important;
-`
+  font-size: 18px !important;
+`;
